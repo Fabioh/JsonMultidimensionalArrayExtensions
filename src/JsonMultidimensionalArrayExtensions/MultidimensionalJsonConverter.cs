ee91136ce20493cdf855b03d4bad2db5e510e13d
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2021-02-10 10:50:45 -0300

    Add project files.

---
+++ src/JsonMultidimensionalArrayExtensions/MultidimensionalJsonConverter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace JsonMultidimensionalArrayExtensions
+{
+    public class MultidimensionalJsonConverter<T> : JsonConverter<T[,]>
+    {
+        public override T[,] Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+
+            if (reader.TokenType != JsonTokenType.StartArray)
+            {
+                throw new JsonException();
+            }
+
+            reader.Read();
+
+            if (reader.TokenType != JsonTokenType.StartArray)
+            {
+                throw new JsonException();
+            }
+
+            reader.Read();
+
+            var elements = new List<List<T>>();
+
+            var innerElements = new List<T>();
+
+            int? qttOfElementsInFirstLine = null;
+
+            while (reader.TokenType != JsonTokenType.EndArray)
+            {
+                while (reader.TokenType != JsonTokenType.EndArray)
+                {
+                    innerElements.Add(JsonSerializer.Deserialize<T>(ref reader, options));
+                    reader.Read();
+                }
+
+                if (!qttOfElementsInFirstLine.HasValue)
+                {
+                    qttOfElementsInFirstLine = innerElements.Count;
+                }
+
+                if (innerElements.Count != qttOfElementsInFirstLine)
+                {
+                    throw new JsonException("the matrix must have teh same qauantity of elements in each line");
+                }
+
+                elements.Add(innerElements);
+
+                reader.Read();
+                if (reader.TokenType != JsonTokenType.StartArray && reader.TokenType != JsonTokenType.EndArray)
+                {
+                    throw new JsonException();
+                }
+                innerElements = new List<T>();
+                reader.Read();
+            }
+
+            var ret = new T[elements.Count, qttOfElementsInFirstLine.Value];
+
+            for (int i = 0; i < elements.Count; i++)
+            {
+                for (int j = 0; j < elements[i].Count; j++)
+                {
+                    ret[i, j] = elements[i][j];
+                }
+            }
+
+            return ret;
+        }
+
+        public override void Write(Utf8JsonWriter writer, T[,] value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/JsonmultidimensionalArrayExtesnsions.Tests/MultidimensionalJsonConverterTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using JsonMultidimensionalArrayExtensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using Xunit;
+
+namespace JsonmultidimensionalArrayExtesnsions.Tests
+{
+    public class MultidimensionalJsonConverterTests
+    {
+        [Fact]
+        public void ShouldSerializeAJsonTextCorrectly()
+        {
+
+            var serializeOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Converters =
+                {
+                    new MultidimensionalJsonConverter<double>()
+                }
+            };
+
+            var jsonString = "[[1,2,3],[1,2,3]]";
+            var v = JsonSerializer.Deserialize<double[,]>(jsonString, serializeOptions);
+
+            v.Should().BeEquivalentTo(new double[,] { { 1, 2, 3 }, { 1, 2, 3 } }); 
+        }
+    }
+}

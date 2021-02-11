040647bec3cec02e662a3bc576c05ee439f62e5b
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2021-02-11 09:44:41 -0300

    - More tests to encrease code coverage

--- tests/JsonmultidimensionalArrayExtesnsions.Tests/MultidimensionalJsonConverterTests.cs
+++ tests/JsonmultidimensionalArrayExtesnsions.Tests/MultidimensionalJsonConverterTests.cs
@@ -28,5 +28,25 @@ namespace JsonmultidimensionalArrayExtesnsions.Tests
 
             v.Should().BeEquivalentTo(new double[,] { { 1, 2, 3 }, { 1, 2, 3 } }); 
         }
+
+        [Fact]
+        public void ShoudThrowExceptionWhenInvalidJsonPassed()
+        {
+            var serializeOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Converters =
+                {
+                    new MultidimensionalJsonConverter<double>()
+                }
+            };
+
+            var jsonString = "[[1,2,3],[1,2,3], [1,2]]";
+            Action act = () => JsonSerializer.Deserialize<double[,]>(jsonString, serializeOptions);
+
+            act.Should()
+                .Throw<JsonException>()
+                .WithMessage("the matrix must have teh same qauantity of elements in each line");
+        }
     }
 }
